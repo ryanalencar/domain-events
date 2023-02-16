ae45963e7422f7d1c884e48bfd24abce0abec4c4
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-02-15 21:33:01 -0300

    ✨ feat(infra): entity class to handle domainEvents(clearing and adding)

--- src/core/Entity.ts
+++ src/core/Entity.ts
@@ -1,15 +1,31 @@
 import { randomUUID } from "crypto";
+import { DomainEvent } from "./DomainEvent";
+import { DomainEvents } from "./DomainEvents";
 
 export class Entity<Props> {
   protected readonly _id: string;
   protected props: Props
-
-  get id() {
-    return this._id
-  }
+  protected _domainEvents: DomainEvent[] = []
 
   protected constructor(props: Props, id?: string) {
     this.props = props
     this._id = id ?? randomUUID()
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get domainEvents() {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent) {
+    this._domainEvents.push(domainEvent)
+    DomainEvents.markEntityForDispatch(this)
+  }
+
+  public clearEvents() {
+    this._domainEvents = []
   }
 }
